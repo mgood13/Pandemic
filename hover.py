6cb176816618cdb2bb1bff5c2b353617e2a1d147
Michael Good <michael@Michaels-MacBook-Pro-3.local>
2020-03-03 21:26:00 -0500

    Added more points and expanded the annotation function

--- hover.py
+++ hover.py
@@ -13,27 +13,30 @@ import csv
 import networkx as nx
 np.random.seed(42)
 colors = ['blue', 'yellow', 'red', 'black']
+color_dict = {'blue': 0, 'yellow': 0, 'red': 0, 'black': 0}
+
 
 f = open('Pandemic Locations.csv', encoding='utf-8-sig')
 city_dictionary = {}
+count = 0
 
 with f:
     reader = csv.reader(f)
     for row in reader:
-        city_dictionary[row[1]] = {'Color': row[0], 'Latitude':row[2],'Longitude':row[3],'Population':row[4], 'X':row[5], 'Y':row[6]}
+        count += 1
+        city_dictionary[row[1]] = {'Color': row[0], 'Latitude': row[2], 'Longitude': row[3],
+                                   'Population': row[4], 'X': row[5], 'Y': row[6], 'ID': row[7], 'Connections': row[8],
+                                   'Disease State': color_dict}
     f.close()
 
-
-print("The location of San Fransisco is: ")
-print(city_dictionary['San fransisco']['X'])
-print(city_dictionary['San fransisco']['Y'])
 
 cities = list(city_dictionary.keys())
 x = {'blue': [], 'yellow': [], 'red': [], 'black': []}
 y = {'blue': [], 'yellow': [], 'red': [], 'black': []}
 xmapped = []
 ymapped = []
-names = np.array(cities[0:12])
+names = np.array(cities[0:24])
+
 
 
 for city in cities:
@@ -45,13 +48,14 @@ for city in cities:
     if city_dictionary[city]['Color'] == 'yellow':
         x['yellow'].append(float(city_dictionary[city]['Longitude']))
         y['yellow'].append(float(city_dictionary[city]['Latitude']))
+        xmapped.append(float(city_dictionary[city]['X']))
+        ymapped.append(float(city_dictionary[city]['Y']))
     if city_dictionary[city]['Color'] == 'red':
         x['red'].append(float(city_dictionary[city]['Longitude']))
         y['red'].append(float(city_dictionary[city]['Latitude']))
     if city_dictionary[city]['Color'] == 'black':
         x['black'].append(float(city_dictionary[city]['Longitude']))
         y['black'].append(float(city_dictionary[city]['Latitude']))
-
 
 plt.style.use('dark_background')
 
@@ -64,7 +68,8 @@ ax.imshow(img, extent=[0, 200, 0, 100])
 ax.set_title('WELCOME TO PANDEMIC. OR... THE REAL WORLD')
 #for color in x:
 #    plt.scatter(x[color], y[color], s=2, c=color)
-sc = plt.scatter(xmapped, ymapped, s=4, c='yellow')
+sc = plt.scatter(xmapped[0:12], ymapped[0:12], s=10, c='blue')
+sc1 = plt.scatter(xmapped[13:24], ymapped[13:24], s=10, c='yellow')
 
 annot = ax.annotate("", xy=(0,0), xytext=(20,20),textcoords="offset points",
                     bbox=dict(boxstyle="round", fc="b"),
@@ -72,7 +77,7 @@ annot = ax.annotate("", xy=(0,0), xytext=(20,20),textcoords="offset points",
 annot.set_visible(False)
 
 def update_annot(ind):
-
+    print(ind)
     pos = sc.get_offsets()[ind["ind"][0]]
     annot.xy = pos
 
@@ -86,7 +91,12 @@ def update_annot(ind):
 def hover(event):
     vis = annot.get_visible()
     if event.inaxes == ax:
+        # this line checks if the position of the mouse equals one of the scatter points
+        # cont becomes true if it is
         cont, ind = sc.contains(event)
+        print(cont)
+        print("THis is ind:    ")
+        print(ind)
         if cont:
             update_annot(ind)
             annot.set_visible(True)
@@ -106,28 +116,18 @@ fig.canvas.mpl_connect("motion_notify_event", hover)
 
 plt.axis('off')
 plt.show()
+
 #xtest = x['blue'][0:2]
 #ytest = y['blue'][0:2]
-
 #plt.plot(xtest,ytest,)
 #print(xtest)
-
 #plt.subplots_adjust(left=0.13, bottom=0.04, right=0.90, top=0.96, wspace=0.07, hspace=0.08)
 #mplcursors.cursor(hover=True)
-
-
-
-
-
 #fig, ax = plt.subplots()
 #x = [1,2]
 #y = [3,4]
-
 #ax.scatter(x,y)
 #ax.set_title("Mouse over a point")
-
-
-
 #plt.show()
 #count = 0
 #G = nx.Graph()
@@ -135,11 +135,6 @@ plt.show()
 #    G.add_node(value,pos = (float(city_dictionary[cities[count]]['Longitude']), float(city_dictionary[cities[count]]['Latitude'])))
 #    count += 1
 #    nx.draw(G)
-
-
-    
-
-    
     #print(pos)
 #pos = G.get_node_attribute(G,'pos')
 #nx.draw(G,pos)
